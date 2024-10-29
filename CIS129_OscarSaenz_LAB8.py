127102b74a3cf307921251af83fbfa18b8fd1ed9
OuterLimitsLemon <osaenz@mail.pima.edu>
2024-10-28 22:46:31 -0700

    Update CIS129_OscarSaenz_LAB8.py

--- CIS129_OscarSaenz_LAB8.py
+++ CIS129_OscarSaenz_LAB8.py
@@ -52,9 +52,6 @@ def sanitized():
                         sanitized.append(test)
                         
         #Confirms length of input is greater than 2 letters               
-        if len(sanitized) < 3:
-            print("Your word must have more than 2 letters.\n")
-            continue
         if space_counter > 0:
             print("Please do not leave blank spaces.\n")
             continue
@@ -63,6 +60,9 @@ def sanitized():
             continue
         if special_character_counter > 0:
             print("Please do not enter any special characters.\n")
+            continue
+        if len(sanitized) < 3:
+            print("Your word must have more than 2 letters.\n")
             continue
         return sanitized
              
@@ -81,8 +81,8 @@ def reverser(sanitized_entry):
 #Tests if the user input is a palindrome or not.
 def palindrome_tester(sanitized_entry, right_to_left):
     """ Tests if list is a palindrome or not (only works on letters) """
-    print(sanitized_entry)
-    print(right_to_left)
+    print('Forwards: ',sanitized_entry)
+    print('Reversed: ',right_to_left)
     master_list = []
     master_list = zip(sanitized_entry, right_to_left)
     for sanitized_entry, right_to_left in master_list:
@@ -107,37 +107,38 @@ def try_again():
 
 #Initialize Global Variables
 
+#Controls the "Press any key to continue... " condition
+any_input = ''
 
 #Maintains the 'while' loop until user chooses to end program.        
 yes_or_no = 'true'
 
-print("-------------------------------------------------------\n")
 print()
-print("Welcome to Palindrome Tester!")
-print("           By O-Tech")
-print()
-print("-------------------------------------------------------\n")
-print("We invite you to support our team.")
-print("Please consider making a $500,000 donation to our Patreon today!")
-print()
-print()
+print("From the studio that brought you beloved classics such as:")
+print("  - 'Theater Seating Lab,'")
+print("  - 'Repitition Structures'")
+print("  - 'Hotdog Cookout Lab'\n\n\n")
+any_input = input("Press any key, then 'ENTER' to continue... ")
 
+if any_input != '':
+    print("\n\n-------------------------------------------------------\n")
+    print()
+    print("             Welcome to Palindrome Tester!")
+    print("                      By O-Tech\n\n")
+    print("pal·in·drome, noun\n       a word that reads the same backward as forward.")
 #While loop used to keep the program looping until stopped by user.
-while yes_or_no == 'true':
+    while yes_or_no == 'true':
 
-    print("-------------------------------------------------------\n")    
-    sanitized_entry = sanitized()
-    right_to_left = reverser(sanitized_entry)
-    palindrome_tester(sanitized_entry, right_to_left)
-    yes_or_no = try_again()
-    print("-------------------------------------------------------\n")
+        print("-------------------------------------------------------")    
+        sanitized_entry = sanitized()
+        right_to_left = reverser(sanitized_entry)
+        palindrome_tester(sanitized_entry, right_to_left)
+        yes_or_no = try_again()
+        print("-------------------------------------------------------\n")
 
-
-print("Goodbye! That $500,000 Patreon offer is still on the table btw!")
-print()
-print("Palindrome Tester 2.1.3 *All Rights Reserved")
-print("PPi:1-a 2024")
-print("-------------------------------------------------------\n")
-    
-
+    print()
+    print("Goodbye!")
+    print()
+    print("Palindrome Tester 2.1.3 *All Rights Reserved")
+    print("PPi:1-a 2024")
 
